493a2afc56134af6fc3f2066de6fe1a69c5d1f8c
lmaddalena <mdn.luca@gmail.com>
2020-02-08 14:31:57 +0100

    Updated banner

--- src/GalaxyGuide.App/Program.cs
+++ src/GalaxyGuide.App/Program.cs
@@ -91,18 +91,18 @@ namespace GalaxyGuide.App
             // https://devops.datenkollektiv.de/banner.txt/index.html
 
             System.Console.WriteLine("\n");
-            
-            Console.WriteLine(@"                           _                    _    _      ");
-            Console.WriteLine(@"  /\/\    ___  _ __   ___ | |__    __ _  _ __  | |_ ( ) ___ ");
-            Console.WriteLine(@" /    \  / _ \| '__| / __|| '_ \  / _` || '_ \ | __||/ / __|");
-            Console.WriteLine(@"/ /\/\ \|  __/| |   | (__ | | | || (_| || | | || |_    \__ \");
-            Console.WriteLine(@"\/    \/ \___||_|    \___||_| |_| \__,_||_| |_| \__|   |___/");
-            Console.WriteLine(@"   ___         _      _         _____            ___         _                     ");
-            Console.WriteLine(@"  / _ \ _   _ (_)  __| |  ___  /__   \  ___     / _ \  __ _ | |  __ _ __  __ _   _ ");
-            Console.WriteLine(@" / /_\/| | | || | / _` | / _ \   / /\/ / _ \   / /_\/ / _` || | / _` |\ \/ /| | | |");
-            Console.WriteLine(@"/ /_\\ | |_| || || (_| ||  __/  / /   | (_) | / /_\\ | (_| || || (_| | >  < | |_| |");
-            Console.WriteLine(@"\____/  \__,_||_| \__,_| \___|  \/     \___/  \____/  \__,_||_| \__,_|/_/\_\ \__, |");
-            Console.WriteLine(@"                                                                             |___/ ");
+
+            Console.WriteLine(@"                           _                    _    _          ___         _      _       ");
+            Console.WriteLine(@"  /\/\    ___  _ __   ___ | |__    __ _  _ __  | |_ ( ) ___    / _ \ _   _ (_)  __| |  ___ ");
+            Console.WriteLine(@" /    \  / _ \| '__| / __|| '_ \  / _` || '_ \ | __||/ / __|  / /_\/| | | || | / _` | / _ \");
+            Console.WriteLine(@"/ /\/\ \|  __/| |   | (__ | | | || (_| || | | || |_    \__ \ / /_\\ | |_| || || (_| ||  __/");
+            Console.WriteLine(@"\/    \/ \___||_|    \___||_| |_| \__,_||_| |_| \__|   |___/ \____/  \__,_||_| \__,_| \___|");
+            Console.WriteLine(@" _            _    _               ___         _                                           ");
+            Console.WriteLine(@"| |_   ___   | |_ | |__    ___    / _ \  __ _ | |  __ _ __  __ _   _                       ");
+            Console.WriteLine(@"| __| / _ \  | __|| '_ \  / _ \  / /_\/ / _` || | / _` |\ \/ /| | | |                      ");
+            Console.WriteLine(@"| |_ | (_) | | |_ | | | ||  __/ / /_\\ | (_| || || (_| | >  < | |_| |                      ");
+            Console.WriteLine(@" \__| \___/   \__||_| |_| \___| \____/  \__,_||_| \__,_|/_/\_\ \__, |                      ");
+            Console.WriteLine(@"                                                               |___/ "                      );
 
             System.Console.WriteLine("\n");
             
